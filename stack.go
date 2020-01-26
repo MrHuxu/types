1a99fc9e52931890d9ea9ee5bbb010fd9c11844a
MrHuxu <hxtheone@gmail.com>
2020-01-26 17:52:56 +0800

    fix stack

--- stack.go
+++ stack.go
@@ -1,31 +1,33 @@
 package types
 
 // Stack defines the struct of a FILO stack
-type Stack []interface{}
+type Stack struct{
+	data []interface{}
+}
 
 // Push pushes an item into the stack
-func (s Stack) Push(x interface{}) {
-	s = append(s, x)
+func (s *Stack) Push(x interface{}) {
+	s.data = append(s.data, x)
 }
 
 // Pop pops an item out from the top of the stack
-func (s Stack) Pop() (x interface{}) {
-	if len(s) == 0 {
+func (s *Stack) Pop() (x interface{}) {
+	if len(s.data) == 0 {
 		return
 	}
 
-	x = s[len(s)-1]
-	s = s[0 : len(s)-1]
+	x = s.data[len(s.data)-1]
+	s.data = s.data[0 : len(s.data)-1]
 	return
 }
 
 // Peek returns the item at the top of the stack
 // and doesn't make any change the stack itself
-func (s Stack) Peek() (x interface{}) {
-	if len(s) == 0 {
+func (s *Stack) Peek() (x interface{}) {
+	if len(s.data) == 0 {
 		return
 	}
 
-	x = s[len(s)-1]
+	x = s.data[len(s.data)-1]
 	return
 }
